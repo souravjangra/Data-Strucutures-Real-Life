641c457af9a51bccb5eb94e85d445908d2341464
Sourav Jangra <imsouravjangra@gmail.com>
2020-07-03 19:27:44 +0530

    added splitwise algorithm and text editor projects

--- Travel-Plan-Graphs/graph.js
+++ Travel-Plan-Graphs/graph.js
@@ -1,83 +1,51 @@
-onload = function() {
-	// creating a network
-	const container = document.getElementById('container');
-	const genNew = document.getElementById('generate-graph');
+function createGraph(V,E){
+    // V - Number of vertices in graph
+    // E - Number of edges in graph (u,v,w)
+    let adj_list = []; // Adjacency list
+    for(let i = 0 ; i < V ; i++){
+        adj_list.push([]);
+    }
+    for(let i = 0 ; i < E.length ; i++){
+        adj_list[E[i][0]].push([E[i][1],E[i][2]]);
+        adj_list[E[i][1]].push([E[i][0],E[i][2]]);
+    }
+    return adj_list;
+}
 
-	// initialising the graph options
-	const options = {
-        edges: {
-            labelHighlightBold: true,
-            font: {
-                size: 20
+function djikstra(graph, V, src) {
+    let vis = Array(V).fill(0);
+    let dist = [];
+    for(let i=0;i<V;i++)
+        dist.push([10000,-1]);
+    dist[src][0] = 0;
+
+    for(let i=0;i<V-1;i++){
+        let mn = -1;
+        for(let j=0;j<V;j++){
+            if(vis[j]===0){
+                if(mn===-1 || dist[j][0]<dist[mn][0])
+                    mn = j;
             }
-        },
-        nodes: {
-            font: '12px arial red',
-            scaling: {
-                label: true
-            },
-            shape: 'icon',
-            icon: {
-                face: 'FontAwesome',
-                code: '\uf015',
-                size: 40,
-                color: '#991133',
+        }
+
+        vis[mn] = 1;
+        for(let j=0;j<graph[mn].length;j++){
+            let edge = graph[mn][j];
+            if(vis[edge[0]]===0 && dist[edge[0]][0]>dist[mn][0]+edge[1]){
+                dist[edge[0]][0] = dist[mn][0]+edge[1];
+                dist[edge[0]][1] = mn;
             }
         }
-    };
-    // initialize your network!
-    const network = new vis.Network(container);
-    network.setOptions(options);
-
-    function createData(){
-        const cities = ['Delhi', 'Mumbai', 'Gujarat', 'Goa', 'Kanpur', 'Jammu', 'Hyderabad', 'Bangalore', 'Gangtok', 'Meghalaya'];
-
-        const V = Math.floor(Math.random() * cities.length) + 3;
-        let vertices = [];
-        for(let i=0;i<V;i++){
-            vertices.push({id:i, label: cities[i-1]});
-        }
-
-        let edges = [];
-        for(let i=1;i<V;i++){
-            let neigh = Math.floor(Math.random()*i);
-            edges.push({from: i, to: neigh, color: 'orange',label: String(Math.floor(Math.random()*70)+30)});
-        }
-
-        const data = {
-            nodes: vertices,
-            edges: edges
-        };
-        return data;
     }
 
-    genNew.onclick = function () {
-        let data = createData();
-        network.setData(data);
-    };
-
-    genNew.click();
+    return dist;
 }
 
-function createGraph(V, E) {
-	// V -> No of vertices
-	// E -> Set of Edges in graph
-	let adjacency_list = [];
+let src = 0;
+let V = 9;
+let E = [[0,1,4], [0,7,8], [1,7,11], [1,2,8], [7,8,7], [6,7,1], [2,8,2],
+    [6,8,6], [5,6,2], [2,5,4], [2,3,7], [3,5,14], [3,4,9], [4,5,10]];
 
-	for(let i=0;i<V;i++) {
-		adjacency_list.push([]);
-	}
-
-	for(let i=0;i<E.length;i++) {
-		adjacency_list[E[i][0]-1].push([E[i][1]-1,E[i][2]]);
-		adjacency_list[E[i][1]-1].push([E[i][0]-1,E[i][2]]);
-
-	}
-	return adjacency_list;
-}
-
-const V = 5;
-const Edges = [[1,2,3], [1,4,2], [3,5,1], [3,4,3]];
-
-let graph = createGraph(V, Edges);
-console.log(graph);
+let graph = createGraph(V,E);
+let distances = djikstra(graph,V,0);
+console.log(distances);
